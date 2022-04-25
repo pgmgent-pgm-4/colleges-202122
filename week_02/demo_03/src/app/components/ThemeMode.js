0ef835d7156ca741c9bc0be36e3e6d6ed6c021e6
Philippe De Pauw - Waterschoot <phildp@FVFGV3N1Q05P.local>
2022-04-25 12:03:07 +0200

    Added Demo 03

--- week_02/demo_03/src/app/components/ThemeMode.js
+++ week_02/demo_03/src/app/components/ThemeMode.js
@@ -1,16 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const ThemeMode = () => {
     const [isDarkMode, setIsDarkMode] = useState(true);
+    const [degrees, setDegrees] = useState(0);
 
-    const toggleTheme = () => {
+    const toggleTheme = (ev) => {
         setIsDarkMode(!isDarkMode);
 
-        document.body.style.backgroundColor = (isDarkMode) ? '#000000' : '#ffffff';
+        // document.body.style.backgroundColor = (isDarkMode) ? '#000000' : '#ffffff';
+        document.body.classList.toggle('is-darkmode');
     };
 
+    const handleDegrees = (ev) => {
+        setDegrees(degrees + 6);
+    };
+
+    const linkHandler = (ev) => {
+        ev.preventDefault();
+        console.log('Do some magic with link');
+    };
+
+    useEffect(() => {
+        document.body.style.transform = `rotate(${degrees}deg)`;
+    }, [degrees])
+    
+
     return (
-        <button onClick={() => toggleTheme()}>Toggle</button>
+        <>
+            <button onClick={(ev) => toggleTheme(ev)}>Toggle</button>
+            <a href="http://www.pgm.gent" title="A Link" onClick={(ev) => linkHandler(ev)}>Press Me</a>
+            <button onClick={(ev) => handleDegrees(ev)}>Degrees</button>
+        </>
     );
 };
 
